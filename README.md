b61fdaba31bb2b5c680dec21420a73da6b4777fc
cosyrusty <priyaanyadav@gmail.com>
2023-04-03 11:54:01 +0530

    impl args parser line -A --number ...

--- README.md
+++ README.md
@@ -18,3 +18,55 @@ With no FILE, or when FILE is -, read standard input.
   cargo run -- [FILE]...
   ```
 
+## DESCRIPTION
+
+Concatenate FILE(s) to standard output.
+
+With no FILE, or when FILE is -, read standard input.
+
+```
+-A, --show-all
+      TODO: equivalent to -vET
+      FOR NOW: equivalent to -ET
+
+-b, --number-nonblank
+      number nonempty output lines, overrides -n
+
+TODO
+-e     equivalent to -vE
+
+-E, --show-ends
+      display $ at end of each line
+
+-n, --number
+      number all output lines
+
+-s, --squeeze-blank
+      suppress repeated empty output lines
+
+TODO
+-t     equivalent to -vT
+
+-T, --show-tabs
+      display TAB characters as ^I
+
+-u     (ignored)
+
+TODO: *learn what are non-printing characters
+-v, --show-nonprinting
+      use ^ and M- notation, except for LFD and TAB
+
+--help display this help and exit
+
+--version
+      output version information and exit
+
+```
+
+### EXAMPLES
+
+`cargo run -- f - g`
+* Output f's contents, then standard input, then g's contents.
+
+`cargo run --` 
+* Copy standard input to standard output.
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,17 @@ use std::io::{stdin, stdout, Read, Write};
 
 fn main() {
     let mut files = vec![];
-    for arg in args().skip(1) {
-        if &arg[..] == "-" {
-            let mut stdin = stdin().lock();
-            let mut buffer = Vec::new();
+    let args: Vec<_> = args().collect();
+    let mut options = Options::new();
 
-            if let Ok(_) = stdin.read_to_end(&mut buffer) {
-                files.push(buffer);
-            }
+    for arg in args.iter().skip(1) {
+        if &arg[..] == "-" {
+            files.push(read_stdin());
+            continue;
+        }
+
+        if arg.starts_with("-") && options.parse(&arg) {
+            //
         } else {
             match fs::read(&arg) {
                 Ok(f) => files.push(f),
@@ -20,10 +23,61 @@ fn main() {
         }
     }
 
+    // eprintln!("{:?}", options);
+
     let mut stdout = stdout().lock();
     for f in files {
         if let Err(e) = stdout.write(&f[..]) {
             eprintln!("Err: writing file, err: [{e}]");
         }
     }
+}
+
+#[derive(Debug)]
+pub struct Options {
+    show_all: bool,        // -A --show-all
+    number_nonblank: bool, // -b, number-nonblank , overrides number
+    show_ends: bool,       // -E --show-ends
+    number: bool,          // -n, --number
+    squeeze_blank: bool,   // -s, --squeeze-blank
+    show_tabs: bool,       // -T, --show-tabs
+}
+
+impl Options {
+    pub fn new() -> Self {
+        Self {
+            show_all: false,
+            number_nonblank: false,
+            show_ends: false,
+            number: false,
+            squeeze_blank: false,
+            show_tabs: false,
+        }
+    }
+
+    // true if successfully parsed else false
+    pub fn parse(&mut self, arg: &str) -> bool {
+        match arg {
+            "-A" | "--show-all" => self.show_all = true,
+            "-b" | "--number-nonblank" => self.number_nonblank = true,
+            "-E" | "--show-ends" => self.show_ends = true,
+            "-n" | "--number" => self.number = true,
+            "-s" | "--squeeze-blank" => self.squeeze_blank = true,
+            "-T" | "--show-tabs" => self.show_tabs = true,
+            _ => {
+                false;
+            }
+        }
+        true
+    }
+}
+
+fn read_stdin() -> Vec<u8> {
+    let mut buf = Vec::new();
+    let mut stdin = stdin().lock();
+
+    // TODO: handle err
+    stdin.read_to_end(&mut buf).unwrap();
+
+    buf
 }
